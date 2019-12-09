3518a17a276e743feabbd5ed3d31dd958d61a773
User <User@Users-MacBook-Pro.local>
2019-12-09 20:12:06 +0600

    Создал приложение API,установил DRF,подключил приложение rest_framework.authtoken

--- source/main/settings.py
+++ source/main/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api_v1',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'webapp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    )
+}
