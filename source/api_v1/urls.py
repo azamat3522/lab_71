1b439dd8969e9464ddd8dbf4ee6448a581304c6b
User <User@Users-MacBook-Pro.local>
2019-12-13 20:52:56 +0600

    Done

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import LogoutView
+from rest_framework.routers import DefaultRouter
+
+from .views import LogoutView, QuoteViewSet
 
 router = routers.DefaultRouter()
 
 app_name = 'api_v1'
+
+router = DefaultRouter()
+router.register(r'quote', QuoteViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', obtain_auth_token, name='obtain_auth_token'),
     path('logout/', LogoutView.as_view(), name='api_token_delete'),
+    path('', include(router.urls))
 
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,6 +1,11 @@
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated, SAFE_METHODS
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from api_v1.serializers import QuoteSerializer
+from webapp.models import Quote, QUOTE_APPROOVED
 
 
 class LogoutView(APIView):
@@ -11,3 +16,32 @@ class LogoutView(APIView):
         if user.is_authenticated:
             user.auth_token.delete()
         return Response({'status': 'ok'})
+
+
+class QuoteViewSet(ModelViewSet):
+    queryset = Quote.objects.none()
+    serializer_class = QuoteSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Quote.objects.all()
+        return Quote.objects.filter(status=QUOTE_APPROOVED)
+
+    def get_permissions(self):
+        if self.action not in ['update', 'partial_update', 'destroy']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    @action(methods=['post'], detail=True)
+    def rate_up(self, request, pk=None):
+        quote = self.get_object()
+        quote.rating += 1
+        quote.save()
+        return Response({'id': quote.pk, 'rating': quote.rating})
+
+    @action(methods=['post'], detail=True)
+    def rate_down(self, request, pk=None):
+        quote = self.get_object()
+        quote.rating -= 1
+        quote.save()
+        return Response({'id': quote.pk, 'rating': quote.rating})
